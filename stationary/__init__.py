8e226507d1107dcbab5688859626e89c80171805
Marc Harper <marc.harper@gmail.com>
2015-08-01 23:06:42 -0700

    Expose Cache and stationary_generator

--- stationary/__init__.py
+++ stationary/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 import stationary.utils
 import stationary.processes
 
-from .stationary_ import stationary_distribution
+from .stationary_ import stationary_distribution, Cache, stationary_generator
 from .entropy_rate_ import entropy_rate
